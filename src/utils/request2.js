07013580a0f022fef26aa7eb6ed58ab4bf584755
Liang <13163704403@163.com>
2022-01-14 11:42:19 +0800

    dd

--- src/utils/request2.js
+++ src/utils/request2.js
@@ -5,7 +5,7 @@ import {
 } from 'element-ui'
 import store from '@/store'
 import {
-  getToken
+// getToken
 } from '@/utils/auth'
 var baseUrlApi = ''
 switch (process.env.NODE_ENV) {
@@ -26,7 +26,7 @@ switch (process.env.NODE_ENV) {
 // create an axios instance
 const service = axios.create({
   baseURL: baseUrlApi, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
+  // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 
@@ -66,12 +66,11 @@ service.interceptors.response.use(
 
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 2000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
+      // Message({
+      // message: res.message || 'Error',
+      //  type: 'error',
+      //  duration: 5 * 1000
+      // })
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
         // to re-login
