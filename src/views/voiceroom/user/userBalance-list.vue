b8eb76de6ac0537c5f4705f7473f4ff99b0d94e4
Liang <13163704403@163.com>
2022-04-28 23:27:58 +0800

    dd

--- src/views/voiceroom/user/userBalance-list.vue
+++ src/views/voiceroom/user/userBalance-list.vue
@@ -85,7 +85,49 @@ export default {
         this.list.map(res => {
           res.create_timeText = res.create_time > 0 ? moment(res.create_time * 1000).format('YYYY-MM-DD HH:mm:ss') : ""
 		res.trade_noText = res.genre == 1 ? res.relation_trade_no : res.trade_no
-		res.genreText = res.genre == 1 ? "收入" : "支出"
+		switch (res.relation_type){
+			case 10:
+			res.genreText = "充值"
+				break;
+			case 11:
+			res.genreText = "后台赠送"
+				break;
+			case 20:
+			res.genreText = "送礼（支付）"
+				break;
+			case 30:
+			res.genreText = "背包抽奖"
+				break;
+			case 40:
+			res.genreText = "派对抽奖"
+				break;
+			case 12:
+			res.genreText = "礼物赠送"
+				break;
+			case 13:
+			res.genreText = "派对内抽奖"
+				break;
+			case 14:
+			res.genreText = "公会固定返点"
+				break;
+			case 15:
+			res.genreText = "公会举报惩罚"
+				break;
+			case 16:
+			res.genreText = "公会举报奖励"
+				break;
+			case 17:
+			res.genreText = "公会返点"
+				break;
+			case 100:
+			res.genreText = "礼物收入"
+				break;
+			case 150:
+			res.genreText = "打赏收入"
+				break;
+				
+		}
+		
 		res.balance = res.genre == 1 ? res.balance : "- "+ res.balance
         })
         this.listLoading = false
