83bbf88ab02fe1fd061cf6984d7627d80f86e75d
Liang <13163704403@163.com>
2022-05-10 11:41:53 +0800

    dd

--- src/utils/request2.js
+++ src/utils/request2.js
@@ -5,6 +5,7 @@ import {
 } from 'element-ui'
 import store from '@/store'
 var baseUrlApi = '';
+console.log(process.env.NODE_ENV)
 switch (process.env.NODE_ENV) {
 	case 'development':
 		baseUrlApi = 'http://api.huida.vip'
