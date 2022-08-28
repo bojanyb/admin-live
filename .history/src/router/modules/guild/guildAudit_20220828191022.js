c23aa13fa3afe2e5ad4da29c2102fb8087e3f608
Liang <13163704403@163.com>
2022-08-28 19:21:16 +0800

    g公会厅申请列表命名变更为拍照管理列表，列表新增字段 房间编号，房间标题

--- .history/src/router/modules/guild/guildAudit_20220828191022.js
+++ .history/src/router/modules/guild/guildAudit_20220828191022.js
@@ -33,7 +33,7 @@ const guildAuditRouter = {
 			component: () => import('@/views/voiceroom/guildAudit/guildApplication-list'),
 			name: 'guildApplication-list',
 			meta: {
-				title: '牌照管理列表22',
+				title: '牌照管理列表',
 				noCache: true
 			}
 		},
