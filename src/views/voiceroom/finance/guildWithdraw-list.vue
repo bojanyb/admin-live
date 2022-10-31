5c1e2e4e1ecbfd5ef54e0e64a322c8cab73e4933
huanglimeng <huanglimeng@163.com>
2022-10-31 22:29:08 +0800

    优化 - 增加公会等级 - 24小时房间奖励结算

--- src/views/voiceroom/finance/guildWithdraw-list.vue
+++ src/views/voiceroom/finance/guildWithdraw-list.vue
@@ -183,7 +183,7 @@ export default {
                             if(params.row.status === 1 && params.row.refund_status === 1) {
                                 return h('span', '已支付')
                             } else if(params.row.status === 1 && params.row.refund_status === 2) {
-                                return h('span', '已退款')
+                                return h('span', '已支付（已退款）')
                             } else if(params.row.status === 3) {
                                 return h('span', '未支付')
                             } else if(params.row.status === 4) {
