0b82a4365eafbc929869f45a469295c3f81c031a
liang <13163704403@163.com>
2022-11-02 20:48:38 +0800

    优化

--- src/views/voiceroom/guildAudit/guildRebates/guildMonthWater.vue
+++ src/views/voiceroom/guildAudit/guildRebates/guildMonthWater.vue
@@ -94,7 +94,8 @@
 						minWidth: '240px',
 						render: (h, params) => {
 							let start_time = params.row.time_start ? timeFormat(params.row.time_start, 'YYYY-MM-DD HH:mm:ss', true) : ''
-							let end_time = params.row.time_end ? timeFormat(params.row.time_end, 'YYYY-MM-DD HH:mm:ss', true) : '无'
+							let endTime = this.form.status === 2 ? params.row.time_end : params.row.create_time
+							let end_time = endTime ? timeFormat(endTime, 'YYYY-MM-DD HH:mm:ss', true) : '无'
 							return h('span', `${timeFormat(params.row.time_start, 'YYYY', true)}年第${timeFormat(start_time, 'MM')}月（${start_time}至${end_time}）`)
 						}
 					},
