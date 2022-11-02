1c4f7dca9611b639039ffa56d04acd466a43b733
yyb <yangyiboaiyi@163.com>
2022-11-02 19:46:10 +0800

    fix: 公会类型修复

--- src/request/index.js
+++ src/request/index.js
@@ -242,7 +242,7 @@ const request = {
         delSettlementConfig: `${HTTPHEADER}/guild/delSettlementConfig`, // 删除周返点配置
         doSettlement: `${HTTPHEADER}/guild/doSettlement`, // 公会批量结算
         getNowRoomOnlineReward: `${HTTPHEADER}/guild/getNowRoomOnlineReward`, // 待结算 24小时房间奖励 
-        guildType: `${HTTPHEADER}/guild/guildType`, // 获取工会类型
+        guildType: `${HTTPHEADER}/guild/guildType`, // 获取公会类型
     },
 
     // 封禁记录
--- src/views/voiceroom/guildAudit/components/editComp.vue
+++ src/views/voiceroom/guildAudit/components/editComp.vue
@@ -179,7 +179,7 @@ export default {
         closed() {
             this.$emit('destoryComp')
         },
-        // 获取工会类型
+        // 获取公会类型
         async getTypeList() {
          const response = await getGuildType()
          if(response.code === 2000) {
