086fb09c3c02abac79d370531b11eb80acd54045
liang <13163704403@163.com>
2022-12-09 18:13:33 +0800

    1、公会列表新增直播公会列表，公会申请列表新增 字段 申请公会类型；派对公会/直播公会 新增修改弹框去掉公会类型字段。2、公会成员 新增直播主播 主播申请列表 新增字段公会类型。3、公会等级 公会周奖励/月奖励配置 新增房间类型。4、新增直播结算 以上未对接接口

--- src/views/voiceroom/guildAudit/guild-list.vue
+++ src/views/voiceroom/guildAudit/guild-list.vue
@@ -10,19 +10,26 @@
 import menuComp from '@/components/menuComp/index.vue'
 // 引入公会列表组件
 import guildListComp from './components/guild-listComp.vue'
+// 引入公会直播列表组件
+import guildLiveComp from './components/guild-liveComp.vue'
 // 引入公会申请列表组件
 import guildListApplyComp from './components/guild-list-applyComp.vue'
 export default {
 	components: {
 		menuComp,
 		guildListComp,
-		guildListApplyComp
+		guildListApplyComp,
+    guildLiveComp
 	},
 	data() {
 		return {
 			menuList: [
                 {
-                    name: '公会列表'
+                    // name: '公会列表'
+                    name: '派对公会'
+                },
+                {
+                    name: '直播公会'
                 },
                 {
                     name: '公会申请列表'
@@ -33,7 +40,21 @@ export default {
 	},
 	computed: {
 		comp() {
-			return this.tabIndex === '0' ? 'guildListComp' : 'guildListApplyComp'
+      let nav = ""
+      switch (this.tabIndex) {
+        case "0":
+        nav = "guildListComp"
+          break;
+        case "1":
+        nav = "guildLiveComp"
+          break;
+        case "2":
+        nav = "guildListApplyComp"
+          break;
+        default:
+          break;
+      }
+			return nav
 		}
 	}
 }
