71e2e6199bc20a748132df2833bdfa167169fb80
yyb <yangyiboaiyi@163.com>
2023-03-07 20:10:35 +0800

    feat: 新人广场

--- src/views/voiceroom/recommend/newbie-square.vue
+++ src/views/voiceroom/recommend/newbie-square.vue
@@ -1,85 +1,38 @@
 <template>
   <div class="recommend-box">
-    <div class="searchParams">
-      <SearchPanel
-        v-model="searchParams"
-        :forms="forms"
-        :show-reset="true"
-        :show-search-btn="true"
-        @onReset="reset"
-        @onSearch="onSearch"
-      ></SearchPanel>
-    </div>
 
-    <tableList :cfgs="cfgs" ref="tableList"></tableList>
+    <tableList :cfgs="cfgs" ref="tableList" @saleAmunt="saleAmunt"></tableList>
 
-    <!-- 详情组件 -->
-    <component
-      v-if="isDestoryComp"
-      :ref="Comp"
-      :is="Comp"
-      @destoryComp="destoryComp"
-    ></component>
   </div>
 </template>
 
 <script>
-// 引入详情组件
-import bindAnchorComp from "./components/bindAnchorComp.vue";
-import bindUserComp from "./components/bindUserComp.vue";
-// 引入菜单组件
-import SearchPanel from "@/components/SearchPanel/final.vue";
 // 引入列表组件
 import tableList from "@/components/tableList/TableList.vue";
 // 引入公共参数
 import mixins from "@/utils/mixins.js";
 // 引入api
 import REQUEST from "@/request/index.js";
-// 引入公共方法
-import { timeFormat } from "@/utils/common.js";
+// 引入公共map
+import MAPDATA from '@/utils/jsonMap.js'
 export default {
   components: {
     tableList,
-    SearchPanel,
-    bindAnchorComp,
-    bindUserComp,
   },
   mixins: [mixins],
   computed: {
     forms() {
-      return [
-        {
-          name: "isTopup",
-          type: "select",
-          value: "",
-          keyName: "value",
-          optionLabel: "name",
-          label: "是否充值",
-          placeholder: "请选择",
-          options: this.topupList,
-        },
-      ];
+      return [];
     },
     cfgs() {
       return {
         vm: this,
-        url: REQUEST.userHistory.recommender,
+        url: REQUEST.userHistory.NewUserStat,
         columns: [
           {
             label: "注册时间",
             width: "180px",
-            render: (h, params) => {
-              return h(
-                "span",
-                params.row.create_time
-                  ? timeFormat(
-                      params.row.create_time,
-                      "YYYY-MM-DD HH:mm:ss",
-                      true
-                    )
-                  : ""
-              );
-            },
+            prop: "create_time"
           },
           {
             label: "ID",
@@ -95,27 +48,25 @@ export default {
           },
           {
             label: "性别",
-            prop: "today_gain",
+            prop: "sex",
+            render: (h, params) => {
+              let data = MAPDATA.SEXLIST.find((item) => {
+                return item.value === params.row.sex;
+              });
+              return h("span", data ? data.name : "未知");
+            },
           },
           {
             label: "地区",
-            prop: "total_gain",
+            prop: "addr",
           },
           {
             label: "充值金额",
-            prop: "today_gain",
+            prop: "recharge",
           },
           {
             label: "消费金额",
-            prop: "total_gain",
-          },
-          {
-            label: "所属公会",
-            prop: "today_gain",
-          },
-          {
-            label: "是否厅主",
-            prop: "total_gain",
+            prop: "consume",
           },
         ],
       };
@@ -123,13 +74,9 @@ export default {
   },
   data() {
     return {
-      isDestoryComp: true, // 是否销毁组件
-      Comp: "", // 组件名称
-      topupList: [
-        { name: "全部", value: 0 },
-        { name: "是", value: 1 },
-        { name: "否", value: 2 },
-      ],
+      searchParams: {
+        query_id: 0
+      }
     };
   },
   methods: {
@@ -143,7 +90,7 @@ export default {
       return {
         page: params.page,
         pagesize: params.size,
-        isTopup: s.isTopup,
+        query_id: s.query_id,
       };
     },
     // 重置
@@ -155,10 +102,14 @@ export default {
     onSearch() {
       this.getList();
     },
-    // 销毁组件
-    destoryComp() {
-      this.isDestoryComp = false;
-    },
+	  // table 返回数据
+    saleAmunt(row) {
+      if (+row.page === 1) {
+        this.searchParams.query_id
+        return false
+      }
+      this.searchParams.query_id = row.query_id;
+	  },
   },
 };
 </script>
