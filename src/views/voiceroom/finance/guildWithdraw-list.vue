990ac8ff235a81954c947f77edc471f13961cf7e
yyb <yangyiboaiyi@163.com>
2023-04-20 15:26:37 +0800

    fixbug

--- src/views/voiceroom/finance/guildWithdraw-list.vue
+++ src/views/voiceroom/finance/guildWithdraw-list.vue
@@ -281,9 +281,8 @@ export default {
             minWidth: "430px",
             render: (h, params) => {
               const data = MAPDATA.IDENTIFICATION.find((item) => {
-                return item.value === params.row.wx_merchant_status ? params.row.wx_merchant_status : params.row.ali_merchant_status;
+                return item.value === (params.row.wx_merchant_status >= 0) ? params.row.wx_merchant_status : params.row.ali_merchant_status;
               });
-              // return (data && params.row.buyer_id && params.row.wx_merchant) ? (
               return (data && params.row.buyer_id) ? (
                 <div style="text-align: left;" title={data.name}>
                   <el-tag type={data.type}>
