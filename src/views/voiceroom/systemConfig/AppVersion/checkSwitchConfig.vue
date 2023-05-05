a21275efdf02f250d928202b0773e9aad6a936f4
liang <13163704403@163.com>
2023-05-05 11:19:26 +0800

    feat: 优化

--- src/views/voiceroom/systemConfig/AppVersion/checkSwitchConfig.vue
+++ src/views/voiceroom/systemConfig/AppVersion/checkSwitchConfig.vue
@@ -56,7 +56,13 @@ export default {
           isActive: false,
           label: "开黑语音",
           value: "com.hdb.kaihei",
-        }
+        },
+        {
+          key: "com.aiyi.lemon",
+          isActive: false,
+          label: "柠檬语音",
+          value: "com.aiyi.lemon",
+        },
       ],
       packageName: [
         {
