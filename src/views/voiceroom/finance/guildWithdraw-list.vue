108db66a3cc4e3dc879dc4eb7354136744b19e9b
yyb <yangyiboaiyi@163.com>
2023-05-24 19:56:13 +0800

    feat: 查询充值订单支付状态

--- src/views/voiceroom/finance/guildWithdraw-list.vue
+++ src/views/voiceroom/finance/guildWithdraw-list.vue
@@ -44,7 +44,7 @@
 
 <script>
 // 引入api
-import { diamondRechargeAll, getMerchantList, wxMerchantList } from "@/api/finance.js";
+import { diamondRechargeAll, getMerchantList, wxMerchantList, queryPayStatus } from "@/api/finance.js";
 // 引入列表组件
 import tableList from "@/components/tableList/TableList.vue";
 // 引入菜单组件
@@ -656,9 +656,14 @@ export default {
           })
       }
     },
-    // 查单
-    handleQueryOrder(row) {
-      console.log(row, "row");
+    // 查询充值订单支付状态
+    async handleQueryOrder(row) {
+      const response = await queryPayStatus({ trade_no: row.trade_no })
+      this.$notify.warning({
+        title: '消息',
+        message: response.data && response.data.list[0].msg,
+      });
+      this.getList();
     }
   },
   created() {
