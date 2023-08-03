0b765084d344bf040102682a5c94db721aae783d
江阳小道 <oyjt001@gmail.com>
2023-08-03 14:19:00 +0800

    feat: aPP底部一周年配置接口调试

--- src/views/voiceroom/systemConfig/AppTabbarConfig.vue
+++ src/views/voiceroom/systemConfig/AppTabbarConfig.vue
@@ -1,203 +1,200 @@
 <template>
-	<div class="app-container">
-
-		<div class="searchParams">
-            <!-- <SearchPanel v-model="searchParams" :forms="forms" :show-reset="true" :show-search-btn="true" :show-add="this.permissionArr.includes('Gift@addGift')" @onReset="reset" @onSearch="onSearch" @add="add"></SearchPanel> -->
+  <div class="app-tabbar-config-box app-container">
+    <div class="searchParams">
+      <div class="formBox">
+        <div class="sunBox">
+          <span>时间选择</span>
+          <el-date-picker
+            v-model="dateTimeParams"
+            type="datetimerange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            value-format="timestamp"
+            :default-time="['00:00:00', '23:59:59']"
+            @change="changeDate"
+          >
+          </el-date-picker>
         </div>
+		<div class="refreshBox">
+		<el-switch
+			v-model="isRefresh"
+			active-text="启用"
+			inactive-text="停用"
+			@change="switchChange"
+		>
+		</el-switch>
+        </div>
+      </div>
+    </div>
 
-		<tableList :cfgs="cfgs" ref="tableList"></tableList>
+    <tableList :cfgs="cfgs" ref="tableList" :is-hide-page="true"></tableList>
 
-		<!-- 新增 - 修改组件 -->
-		<bannerComp v-if="isDestoryComp" ref="bannerComp" @destoryComp="destoryComp" @getList="getList"></bannerComp>
-	</div>
+    <!-- 修改组件 -->
+    <tabbarComp
+      v-if="isDestoryComp"
+      ref="tabbarComp"
+      @destoryComp="destoryComp"
+      @getList="getList"
+    ></tabbarComp>
+  </div>
 </template>
 
 <script>
-	// 引入api
-	import { getBannerChange } from '@/api/videoRoom'
-	// 引入新增 - 修改组件
-	import bannerComp from './bannerComp/index.vue'
-	// 引入列表组件
-	import tableList from '@/components/tableList/TableList.vue'
-	// 引入api
-	import REQUEST from '@/request/index.js'
-	// 引入公共方法
-	import { timeFormat } from '@/utils/common.js'
-	// 引入公共参数
-	import mixins from '@/utils/mixins.js'
-	// 引入公共map
-	import MAPDATA from '@/utils/jsonMap.js'
-	export default {
-		name: 'banner-list',
-		mixins: [mixins],
-		components: {
-			bannerComp,
-			tableList
-		},
-		data() {
-			return {
-				isDestoryComp: false, // 是否销毁组件
-			}
-		},
-		computed: {
-			StartPicker() {
-				return {
-					disabledDate: (time) => {
-						return time.getTime() < new Date().getTime() - 24 * 60 * 60 * 1000;
-					}
-				}
-			},
-			EndPicker() {
-				return {
-					disabledDate: (time) => {
-						return time.getTime() < new Date().getTime() - 24 * 60 * 60 * 1000;
-					}
-				}
-			},
-			startTime() {
-				let start = this.popForm.start_time
-				let now = new Date().getTime()
-				let params = {}
-				params = {
-					required: true,
-					validator: (rules, val, cb) => {
-						if(start < now && this.type === 'Add') {
-							cb(new Error('开始时间不可小于当前时间'))
-						} else {
-							cb()
-						}
-					}
-				}
-				return params
-			},
-			endTime() {
-				let start = this.popForm.start_time
-				let now = new Date().getTime()
-				let params = {}
-				params = {
-					required: true,
-					validator: (rules, val, cb) => {
-						if(start < now && this.type === 'Add') {
-							cb(new Error('结束时间不可小于当前时间'))
-						} else {
-							cb()
-						}
-					}
-				}
-				return params
-			},
-			cfgs() {
-				return {
-					vm: this,
-					url: REQUEST.system.tabbar.getConfigList,
-					columns: [
-						{
-							label: '序号',
-							prop: 'sort'
-						},
-						{
-							label: 'banner标题',
-							prop: 'title'
-						},
-						{
-							label: '轮播顺序',
-							prop: 'sort'
-						},
-						{
-							label: 'banner图片',
-							isimg: true,
-							prop: 'pic',
-							imgHeight: '50px'
-						},
-						{
-							label: 'banner类型',
-							render: (h, params) => {
-								let data = MAPDATA.BANNERSYSTEMTYPELIST.find(item => { return item.value === params.row.type })
-								return h('span', data ? data.name : '无')
-							}
-						},
-						{
-							label: '更新时间',
-							render: (h, params) => {
-								return h('span', params.row.update_time ? timeFormat(params.row.update_time, 'YYYY-MM-DD HH:mm:ss', true) : '无')
-							}
-						},
-						{
-							label: '启用/禁用',
-							isSwitch: true,
-							prop: 'status',
-							isTrueValue: 1,
-							isFalseValue: 2,
-							change: (v, row) => {
-								this.changeSwitch(row.id, v)
-							}
-						},
-						{
-							label: '操作',
-							render: (h, params) => {
-								return h('div', [
-									h('el-button', { props: { type: 'primary'}, on: {click:()=>{this.update(params.row)}}}, '修改')
-								])
-							}
-						}
-					]
-				}
-			}
-		},
-		methods: {
-			// 配置参数
-			beforeSearch(params) {
-				return {
-					page: params.page,
-					pagesize: params.size
-				}
-			},
-			// 刷新列表
-			getList() {
-				this.$refs.tableList.getData()
-			},
-			// 重置
-			reset() {
-				this.searchParams = {}
-				this.getList()
-			},
-			// 查询
-			onSearch() {
-				this.getList()
-			},
-			// 修改
-			update(row) {
-				this.load('update', row)
-			},
-			load(status, row) {
-				this.isDestoryComp = true
-				setTimeout(() => {
-					this.$refs.bannerComp.loadParams(status, row)
-				}, 50);
-			},
-			// 销毁组件
-			destoryComp() {
-				this.isDestoryComp = false
-			},
-			// 更改
-			async changeSwitch(id, status) {
-				let params = {
-					id,
-					status: status === 1 ? true : false
-				}
-				let res = await getBannerChange(params)
-				if(res.code === 2000) {
-					if(status === 1) {
-						this.$message.success('启用成功')
-					} else {
-						this.$message.success('禁用成功')
-					}
-					this.getList()
-				}
-			}
+// 引入api
+import { updateTabbarStatus } from '@/api/system'
+// 引入修改组件
+import tabbarComp from "./tabbarComp/index.vue";
+// 引入列表组件
+import tableList from "@/components/tableList/TableList.vue";
+// 引入api
+import REQUEST from "@/request/index.js";
+// 引入公共方法
+import { timeFormat } from "@/utils/common.js";
+// 引入公共参数
+import mixins from "@/utils/mixins.js";
+export default {
+  name: "banner-list",
+  mixins: [mixins],
+  components: {
+    tabbarComp,
+    tableList,
+  },
+  data() {
+    return {
+      isDestoryComp: false, // 是否销毁组件
+      isRefresh: false, // 是否启用
+      dateTimeParams: [],
+    };
+  },
+  computed: {
+    cfgs() {
+      return {
+        vm: this,
+        url: REQUEST.system.tabbar.getConfigList,
+        columns: [
+          {
+            label: "位置",
+            prop: "id",
+          },
+          {
+            label: "文字",
+            prop: "name",
+          },
+          {
+            label: "图片",
+            prop: "icon",
+			isimg: true,
+			imgWidth: '50px',
+			imgHeight: '50px'
+          },  
+          {
+            label: "操作",
+            render: (h, params) => {
+              return h("div", [
+                h(
+                  "el-button",
+                  {
+                    props: { type: "primary" },
+                    on: {
+                      click: () => {
+                        this.update(params.row);
+                      },
+                    },
+                  },
+                  "修改"
+                ),
+              ]);
+            },
+          },
+        ],
+      };
+    },
+  },
+  methods: {
+    // 配置参数
+    beforeSearch(params) {
+      return {
+        page: params.page,
+        pagesize: params.size,
+      };
+    },
+	// 请求完回调
+	onSearchSuccess(res) {
+		const data = res.data;
+		return {
+			data: data.list || [],
 		}
-	}
+	},
+    // 刷新列表
+    getList() {
+      this.$refs.tableList.getData();
+    },
+    // 修改
+    update(row) {
+      this.load("update", row);
+    },
+    load(status, row) {
+      this.isDestoryComp = true;
+      setTimeout(() => {
+        this.$refs.tabbarComp.loadParams(status, row);
+      }, 50);
+    },
+    // 销毁组件
+    destoryComp() {
+      this.isDestoryComp = false;
+    },
+    // 日期更改
+    changeDate() {},
+    // 是否启用配置
+    switchChange(value) {
+		const params = {
+			type: 1, // 类型: 1- 更新状态/2-更新生效时间
+			status: value ? 0 : 1
+		}
+		updateTabbarStatus().then(res => {
+			if(res.code !== 20000) return;
+			this.$success("已开启");
+		})
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+.app-tabbar-config-box {
+  .formBox {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    .sunBox {
+      margin-right: 12px;
+      margin-bottom: 20px;
+      > span {
+        font-size: 14px;
+        color: #606266;
+        font-weight: 700;
+        margin-right: 12px;
+      }
+      .el-input {
+        width: 180px;
+        input {
+          border: none;
+          background: #f5f7fa;
+        }
+      }
+      .el-date-editor {
+        border: none;
+        background: #f5f7fa;
+        input {
+          background: #f5f7fa;
+        }
+      }
+    }
+	.refreshBox {
+		margin-bottom: 20px;
+		margin-left: 10px;
+	}
+  }
+}
 </style>
---
+++ src/views/voiceroom/systemConfig/tabbarComp/index.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="tabbarComp-box">
+        <el-dialog
+        :title="titie"
+        :visible.sync="dialogVisible"
+        width="450px"
+        :before-close="handleClose"
+        @closed="closed">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="130px" class="demo-ruleForm">
+                <el-form-item label="文字" prop="name">
+                    <el-input v-model="ruleForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="文字选中后变色的色值" prop="color">
+                    <el-input v-model="ruleForm.color"></el-input>
+                </el-form-item>
+                <el-form-item label="图片" prop="icon">
+                    <uploadImg ref="uploadImg" v-model="ruleForm.icon" :imgUrl="ruleForm.icon" name="icon" @validateField="validateField" accept=".png"></uploadImg>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+// 引入api
+import { updateTabbarConfig } from '@/api/system'
+// 引入图片上传组件
+import uploadImg from '@/components/uploadImg/index.vue'
+export default {
+    components: {
+        uploadImg
+    },
+    computed: {
+        titie() {
+            return '位置'
+        }
+    },
+    data() {
+        return {
+            dialogVisible: false,
+            ruleForm: {
+                name: '',
+                color: '',
+                icon: '',
+            },
+            rules: {
+                name: [
+                    { required: true, message: '请输入文字', trigger: 'blur' }
+                ],
+                color: [
+                    { required: true, message: '请输入文字选中后变色的色值', trigger: 'blur' }
+                ],
+                icon: [
+                    { required: true, message: '请上传图片', trigger: 'change' }
+                ]
+            }
+        };
+    },
+    methods: {
+        handleClose() {
+            this.dialogVisible = false
+        },
+        // 获取数据
+        loadParams(status, row) {
+            this.dialogVisible = true
+            let params = JSON.parse(JSON.stringify(row))
+            this.$set(this.$data, 'ruleForm', params)
+        },
+        // 提交
+        async submitForm(formName) {
+            this.$refs[formName].validate(async (valid) => {
+                if (valid) {
+                    let s = this.ruleForm
+                    let params = {
+                        id: s.id,
+                        name: s.title,
+                        color: s.pic,
+                        icon: s.type,
+                    }
+                    let res = await updateTabbarConfig(params)
+                    if(res.code === 2000) {
+                        this.$message.success('操作成功！')
+                        this.dialogVisible = false
+                        this.$emit('getList')
+                    }
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+        // 重置
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        },
+        // 销毁组件
+        closed() {
+            this.$emit('destoryComp')
+        },
+        // 重置字段验证
+        validateField(name) {
+            this.$refs.ruleForm.validateField([name])
+        }
+    }
+}
+</script>
