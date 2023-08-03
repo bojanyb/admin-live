0f4a0878c72af4742638d844a5acf8dd3f0f47a8
江阳小道 <oyjt001@gmail.com>
2023-08-03 18:10:06 +0800

    fix: 代码合并

--- src/request/index.js
+++ src/request/index.js
@@ -212,7 +212,7 @@ const request = {
     user: {
         list: `${HTTPHEADER}/User/getUserInfo`,
         defaultFace: `${HTTPHEADER}/user/defaultFace`,
-        autonymlist: `${HTTPHEADER}/Verify`,
+        autonymlist: `${HTTPHEADER}/Verify/list`,
         getVerifyOptions: `${HTTPHEADER}/Verify/options`, // 获取实名列表筛选项
         riskList: `${HTTPHEADER}/Verify/rbiList`, // 风险列表
         edit: `${HTTPHEADER}/User/userEdit`, // 编辑用户
