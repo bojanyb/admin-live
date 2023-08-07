96f6f86cbde3b94047473d89f66cbeecef1c99dd
江阳小道 <oyjt001@gmail.com>
2023-08-07 19:37:48 +0800

    feat: 移除字符串所有的空格

--- src/views/voiceroom/systemConfig/superAdmin/add/messageComp.vue
+++ src/views/voiceroom/systemConfig/superAdmin/add/messageComp.vue
@@ -56,7 +56,7 @@ export default {
                     let s = this.ruleForm
                     let params = {
                         kv_id: s.kv_id,
-                        kv_value: s.kv_value
+                        kv_value: s.kv_value.replace(/\s*/g,'')
                     }
                     let res = await update(params)
                     if(res.code === 2000) {
